df6cedd3d70534eaf4812e4a69dce13d1584545c
Isa <isamitre@gmail.com>
2021-03-10 00:13:30 -0500

    removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import logo from './logo.svg';
-import 'semantic-ui-css/semantic.min.css'
+import logo from "./logo.svg";
+import "semantic-ui-css/semantic.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 
-import Home from './components/Home';
-import About from './components/About';
-import Calendar from './components/Calendar';
-import BookRecs from './components/BookRecs';
+import Home from "./components/Home";
+import About from "./components/About";
+import Calendar from "./components/Calendar";
+import BookRecs from "./components/BookRecs";
 
 function App() {
   return (
     <div>
       <Switch>
-          <Route path="/" component={Home} exact/>
-          <Route path="/About" component={About} exact/>
-          <Route path="/Calendar" component={Calendar} exact/>
-          <Route path="/BookRecs" component={BookRecs} exact/>
+        <Route path="/" component={Home} exact />
+        <Route path="/About" component={About} exact />
+        <Route path="/Calendar" component={Calendar} exact />
+        <Route path="/BookRecs" component={BookRecs} exact />
       </Switch>
     </div>
   );
