d65fa43543ab1a2e37ea0d5de4f6a40fbc6b7cc5
Matthew Cohen <cohen.matthew@ufl.edu>
2021-03-15 17:41:47 -0400

    Completed get and post methods for votebooks

--- src/services/voteBook.service.js
+++ src/services/voteBook.service.js
@@ -9,11 +9,18 @@ var voteBookAxiosInstance = axios.create({
 
 class VoteBookService() {
   getVoteBooks() {
-
+    return openlibraryAxiosInstance.get("localhost:8080/api/voteBooks/");
   }
 
   createVoteBook(title, isbn, isSpoilerBook) {
     let voteCount = 0;
+    let data = {
+      title: title,
+      isbn: isbn,
+      isSpoilerBook: isSpoilerBook,
+      voteCount: voteCount
+    }
+    return openlibraryAxiosInstance.post("localhost:8080/api/voteBooks/", data);
   }
 
   updateVoteBookCount(isbn, newCount) {
