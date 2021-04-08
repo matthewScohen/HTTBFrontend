c54d6a1a9e0000903e5fc04ff2ddea7449d3cad9
Matthew Cohen <cohen.matthew@ufl.edu>
2021-04-08 13:11:14 -0400

    Added functionality to add/remove votebooks from the admin panel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -3,6 +3,7 @@ import { Button, Form } from "semantic-ui-react";
 import ".././App.css";
 import '../css/AdminPanel.css';
 import CalendarService from "../services/calendar.service";
+import VoteBookService from "../services/voteBook.service";
 
 import { sideMenu as Menu } from "./sideMenu.js";
 
@@ -12,6 +13,9 @@ function AdminPanel() {
   const [addEventTitle, setAddEventTitle] = useState("");
   const [deleteEventTitle, setDeleteEventTitle] = useState("");
   const [addEventDate, setAddEventDate] = useState("");
+
+  const [voteBookTitle, setVoteBookTitle] = useState("");
+  const [voteBookIsbn, setVoteBookIsbn] = useState("");
 
   async function handleAddEvent() {
     var password = prompt("Please enter the admin password", "Password");
@@ -39,49 +43,86 @@ function AdminPanel() {
     setDeleteEventTitle("");
   }
 
+  async function handleAddVoteBook() {
+    console.log(voteBookTitle, voteBookIsbn);
+    const response = await (VoteBookService.createVoteBook(voteBookTitle, voteBookIsbn, false));
+    if(response.data.isbn == voteBookIsbn)
+      alert("Book added to poll successfully!");
+    else
+      alert("There was an error adding the book to the poll.");
+  }
+
+  async function handleRemoveVoteBook() {
+    console.log(voteBookTitle, voteBookIsbn);
+    const response = await (VoteBookService.deleteVoteBook(voteBookIsbn));
+    alert(response.data.message);
+  }
+
   return (
     //The outer-container contains everything including the menu
     //The page wrap must contain everything on the page except the menu
     <div id="outer-container">
       <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
       <div id="page-wrap">
-      <center>
-        <h1>Admin Panel</h1>
-      </center>
-      <h2>Add event to calendar</h2>
-      <Form onSubmit={handleAddEvent}>
-        <Form.Group widths='equal'>
-          <Form.Input
-                label="Event Title"
-                placeholder='GBM #1'
-                name='title'
-                value={addEventTitle}
-                onChange={(e, { name, value }) => setAddEventTitle(value)}
-          />
-          <Form.Input
-                label="Event Date"
-                placeholder="YYYY-MM-DD"
-                name='date'
-                value={addEventDate}
-                onChange={(e, { name, value }) => setAddEventDate(value)}
-          />
-          <Form.Button content='Add Event' />
-        </Form.Group>
-      </Form>
+        <center>
+          <h1>Admin Panel</h1>
+        </center>
+        <h2>Add event to calendar</h2>
+        <Form onSubmit={handleAddEvent}>
+          <Form.Group widths='equal'>
+            <Form.Input
+                  label="Event Title"
+                  placeholder='GBM #1'
+                  name='title'
+                  value={addEventTitle}
+                  onChange={(e, { name, value }) => setAddEventTitle(value)}
+            />
+            <Form.Input
+                  label="Event Date"
+                  placeholder="YYYY-MM-DD"
+                  name='date'
+                  value={addEventDate}
+                  onChange={(e, { name, value }) => setAddEventDate(value)}
+            />
+            <Form.Button content='Add Event' />
+          </Form.Group>
+        </Form>
 
-      <h2>Delete event from calendar</h2>
-      <Form onSubmit={handleDeleteEvent}>
-        <Form.Group widths='equal'>
-          <Form.Input
-                label="Event Title"
-                placeholder='GBM #1'
-                name='title'
-                value={deleteEventTitle}
-                onChange={(e, { name, value }) => setDeleteEventTitle(value)}
-          />
-          <Form.Button content='Delete Event' />
-        </Form.Group>
-      </Form>
+        <h2>Delete event from calendar</h2>
+        <Form onSubmit={handleDeleteEvent}>
+          <Form.Group widths='equal'>
+            <Form.Input
+                  label="Event Title"
+                  placeholder='GBM #1'
+                  name='title'
+                  value={deleteEventTitle}
+                  onChange={(e, { name, value }) => setDeleteEventTitle(value)}
+            />
+            <Form.Button content='Delete Event' />
+          </Form.Group>
+        </Form>
+
+        <h2> Add/Remove Book To/From Voting Poll </h2>
+        <Form>
+          <Form.Group widths='equal'>
+            <Form.Input
+                  label="Title"
+                  placeholder='The Name of the Wind'
+                  name='title'
+                  value={voteBookTitle}
+                  onChange={(e, { name, value }) => setVoteBookTitle(value)}
+            />
+            <Form.Input
+                  label="ISBN"
+                  placeholder='9781423389262'
+                  name='isbn'
+                  value={voteBookIsbn}
+                  onChange={(e, { name, value }) => setVoteBookIsbn(value)}
+            />
+            <Form.Button onClick={handleAddVoteBook} content="Add Book" />
+            <Form.Button onClick={handleRemoveVoteBook} content="Remove Book" />
+          </Form.Group>
+        </Form>
       </div>
     </div>
   );
