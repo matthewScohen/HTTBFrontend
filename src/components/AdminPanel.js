95afc05fa619d2c3d48281d31c65511553c3f543
digitdustin <dustinkarp52@gmail.com>
2021-04-13 14:23:19 -0400

    Changed styling for admin panel and added tables

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Form } from "semantic-ui-react";
+import { Button, Form, Segment, Table } from "semantic-ui-react";
 import ".././App.css";
 import '../css/AdminPanel.css';
 import CalendarService from "../services/calendar.service";
@@ -82,34 +82,73 @@ function AdminPanel() {
     //The outer-container contains everything including the menu
     //The page wrap must contain everything on the page except the menu
     <div id="outer-container">
+      <div id="admin-outer">
       <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
       <div id="page-wrap">
         <center>
           <h1>Admin Panel</h1>
         </center>
-        <h2>Add event to calendar</h2>
-        <Form onSubmit={handleAddEvent}>
-          <Form.Group widths='equal'>
-            <Form.Input
-                  label="Event Title"
-                  placeholder='GBM #1'
-                  name='title'
-                  value={addEventTitle}
-                  onChange={(e, { name, value }) => setAddEventTitle(value)}
-            />
-            <Form.Input
-                  label="Event Date"
-                  placeholder="YYYY-MM-DD"
-                  name='date'
-                  value={addEventDate}
-                  onChange={(e, { name, value }) => setAddEventDate(value)}
-            />
-            <Form.Button content='Add Event' />
-          </Form.Group>
-        </Form>
+        <div id="forms-container">
+          <div id="add-calendar" className="separate-form">
+          
+          <Segment inverted>
+          <h2>Add event to calendar</h2>
+          <Form inverted onSubmit={handleAddEvent}>
+            <Form.Group widths="equal">
+              <Form.Input
+                    fluid
+                    label="Event Title"
+                    placeholder='GBM #1'
+                    name='title'
+                    value={addEventTitle}
+                    onChange={(e, { name, value }) => setAddEventTitle(value)}
+              />
+              <Form.Input
+                    fluid
+                    label="Event Date"
+                    placeholder="YYYY-MM-DD"
+                    name='date'
+                    value={addEventDate}
+                    onChange={(e, { name, value }) => setAddEventDate(value)}
+              />
+              <Form.Button id="formsubmitbutton" content='Add Event' />
+            </Form.Group>
+          </Form>
+          <Table unstackable selectable striped>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Event Title</Table.HeaderCell>
+                <Table.HeaderCell>Event Date</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+            <Table.Row>
+              {/*Adding an <a> in a selectable column, maybe this can autofill the
+              form components with the selected option?*/}
+              <Table.Cell selectable><a href="#">GBM 1</a></Table.Cell>
+              <Table.Cell>0000-00-00</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>GBM 2</Table.Cell>
+              <Table.Cell>0000-00-00</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>GBM 3</Table.Cell>
+              <Table.Cell>0000-00-00</Table.Cell>
+            </Table.Row>
+            
+          </Table.Body>
+          </Table>
+          
+          </Segment>
+          </div>
+        
 
+        <div id="delete-calendar" className="separate-form">
+          
+          <Segment inverted>
         <h2>Delete event from calendar</h2>
-        <Form onSubmit={handleDeleteEvent}>
+        <Form inverted onSubmit={handleDeleteEvent}>
           <Form.Group widths='equal'>
             <Form.Input
                   label="Event Title"
@@ -118,12 +157,43 @@ function AdminPanel() {
                   value={deleteEventTitle}
                   onChange={(e, { name, value }) => setDeleteEventTitle(value)}
             />
-            <Form.Button content='Delete Event' />
+            <Form.Button id="formsubmitbutton" content='Delete Event' />
           </Form.Group>
         </Form>
+        <Table unstackable selectable striped>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Event Title</Table.HeaderCell>
+                <Table.HeaderCell>Event Date</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+            <Table.Row>
+              {/*Adding an <a> in a selectable column, maybe this can autofill the
+              form components with the selected option?*/}
+              <Table.Cell selectable><a href="#">GBM 1</a></Table.Cell>
+              <Table.Cell>0000-00-00</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>GBM 2</Table.Cell>
+              <Table.Cell>0000-00-00</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>GBM 3</Table.Cell>
+              <Table.Cell>0000-00-00</Table.Cell>
+            </Table.Row>
+            
+          </Table.Body>
+          </Table>
+        </Segment>
+        </div>
+        
 
+        <div id="change-vote" className="separate-form">
+          
+          <Segment inverted>
         <h2> Add/Remove Book To/From Voting Poll </h2>
-        <Form>
+        <Form inverted>
           <Form.Group widths='equal'>
             <Form.Input
                   label="Title"
@@ -139,13 +209,43 @@ function AdminPanel() {
                   value={voteBookIsbn}
                   onChange={(e, { name, value }) => setVoteBookIsbn(value)}
             />
-            <Form.Button onClick={handleAddVoteBook} content="Add Book" />
-            <Form.Button onClick={handleRemoveVoteBook} content="Remove Book" />
+            <Form.Button id="formsubmitbutton" onClick={handleAddVoteBook} content="Add Book" />
+            <Form.Button id="formsubmitbutton" onClick={handleRemoveVoteBook} content="Remove Book" />
           </Form.Group>
         </Form>
+        <Table unstackable selectable striped>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Title</Table.HeaderCell>
+                <Table.HeaderCell>ISBN</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+            <Table.Row>
+              {/*Adding an <a> in a selectable column, maybe this can autofill the
+              form components with the selected option?*/}
+              <Table.Cell selectable><a href="#">Book Title</a></Table.Cell>
+              <Table.Cell>1234567890</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell selectable><a href="#">Book Title</a></Table.Cell>
+              <Table.Cell>1234567890</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell selectable><a href="#">Book Title</a></Table.Cell>
+              <Table.Cell>1234567890</Table.Cell>
+            </Table.Row>
+            
+          </Table.Body>
+          </Table>
+        </Segment>
+        </div>
 
+        <div id="delete-rec" className="separate-form">
+          
+          <Segment inverted>
         <h2> Delete Book From Book Recommendations </h2>
-        <Form onSubmit={handleDeleteBookRec}>
+        <Form inverted onSubmit={handleDeleteBookRec}>
           <Form.Group widths='equal'>
             <Form.Input
                   label="Book ISBN"
@@ -154,12 +254,43 @@ function AdminPanel() {
                   value={bookRecIsbn}
                   onChange={(e, { name, value }) => setBookRecIsbn(value)}
             />
-            <Form.Button content='Delete Event' />
+            <Form.Button id="formsubmitbutton" content='Delete Event' />
           </Form.Group>
         </Form>
+        <Table unstackable selectable striped>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Title</Table.HeaderCell>
+                <Table.HeaderCell>ISBN</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+            <Table.Row>
+              {/*Adding an <a> in a selectable column, maybe this can autofill the
+              form components with the selected option?*/}
+              <Table.Cell selectable><a href="#">Book Title</a></Table.Cell>
+              <Table.Cell>1234567890</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell selectable><a href="#">Book Title</a></Table.Cell>
+              <Table.Cell>1234567890</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell selectable><a href="#">Book Title</a></Table.Cell>
+              <Table.Cell>1234567890</Table.Cell>
+            </Table.Row>
+            
+          </Table.Body>
+          </Table>
+        </Segment>
+        </div>
 
+        
+        <div id="update-spoiler" className="separate-form">
+          
+          <Segment inverted>
         <h2> Update Spoiler Book </h2>
-        <Form onSubmit={handleUpdateSpoilerBook}>
+        <Form inverted onSubmit={handleUpdateSpoilerBook}>
           <Form.Group widths='equal'>
             <Form.Input
                   label="Title"
@@ -175,10 +306,31 @@ function AdminPanel() {
                   value={spoilerBookIsbn}
                   onChange={(e, { name, value }) => setSpoilerBookIsbn(value)}
             />
-            <Form.Button content='Update' />
+            <Form.Button id="formsubmitbutton" content='Update' />
           </Form.Group>
         </Form>
+        <Table unstackable selectable striped>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Title</Table.HeaderCell>
+                <Table.HeaderCell>ISBN</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+            <Table.Row>
+              {/*Adding an <a> in a selectable column, maybe this can autofill the
+              form components with the selected option?*/}
+              <Table.Cell selectable><a href="#">Book Title</a></Table.Cell>
+              <Table.Cell>1234567890</Table.Cell>
+            </Table.Row>
+            
+          </Table.Body>
+          </Table>
+        </Segment>
+        </div>
 
+        </div>
+        </div>
       </div>
     </div>
   );
